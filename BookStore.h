8bcd48e7395790881dd2f32fca2397e53f3b723b
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-21 01:10:54 +0200

    BookStore::new_book()

--- BookStore.h
+++ BookStore.h
@@ -26,6 +26,33 @@ private:
     
 public:
     /**
+     * Adds new book to the system with 'uds' units, or adds 'uds' units
+     * if the book already exists.
+     * Complexity: O(..)
+     *
+     * @param bt Book title
+     * @param uds Units to be added into stock
+     */
+    void new_book(BookTitle bt, unsigned int uds) {
+        
+        // try to find book
+        HashMap<BookTitle, Book>::Iterator it = _books.find(bt);    // O(1)
+        
+        // if it's not present, add it to the system
+        if (it != _books.end()) {
+            _books.insert(bt, Book(bt, uds));                       // O(1)
+            
+            // we also have to update the best-sellers (maybe best-seller has 0 sales...)
+            _best_sellers.update(_books[bt]);
+        }
+        
+        // otherwise, add 'uds' units to its stock
+        else {
+            Book* b = &(it.value());                                // O(1)
+            b->add_stock(uds);                                      // O(1)
+        }
+    }
+    /**
      * Complexity: O(1)
      * @returns true if book is present in the system, even if it is out of stock
      */
