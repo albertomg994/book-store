7056faacd60586de15cc433b5244466f0085e896
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-21 01:09:40 +0200

    Created BestSellers class

--- BestSellers.h
+++ BestSellers.h
@@ -9,5 +9,24 @@
 #ifndef BestSellers_h
 #define BestSellers_h
 
+#include "Book.h"
+#include <list>
+#include <string>
+#include <iostream>
+
+#define BEST_SELLERS_SIZE 10
+
+using namespace std;
+
+/**
+ * Best-sellers class
+ */
+class BestSellers {
+private:
+    
+    list<Book*> _books;
+    
+public:
+};
 
 #endif /* BestSellers_h */
