39d95d647d954c4d33a27c58288cd69f42b6bf3d
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-21 01:08:19 +0200

    Empty main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  T11-Venta-Libros
+//
+//  Created by Alberto Miedes on 20/5/16.
+//  Copyright © 2016 Alberto Miedes. All rights reserved.
+//
+
+#include <iostream>
+#include "BookStore.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
