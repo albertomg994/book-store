2cca12d6238089a67f62dc122dc04080aeaf4cb5
Alberto Miedes Garcés <albertomg994@gmail.com>
2016-05-22 11:31:59 +0200

    Added tests

---
+++ cr.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  cr.sh
+#  T11-Venta-Libros
+#
+#  Created by Alberto Miedes on 21/5/16.
+#  Copyright © 2016 Alberto Miedes. All rights reserved.
+
+g++ -std=c++11 main.cpp -o bookstore
+./bookstore < test/test-1.in > test/test-1.out
